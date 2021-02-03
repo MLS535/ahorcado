441918c5ed78bd02409049da3506c7ea8b2574f9
Maite <mladaria@cifpfbmoll.eu>
2021-02-03 16:59:14 +0100

    He ficat nous metodos

--- com/jaume/penjat/Tauler.java
+++ com/jaume/penjat/Tauler.java
@@ -1,25 +1,63 @@
 package com.jaume.penjat;
 
 public class Tauler {
+    String paraulaSecreta;
+}
+    public boolean hasGuanyat() {
+    }
+
+    public boolean imprimir() {
+    }
 
     public String verificar(String letra) {
         if (letra.length() > 1) {
             return "Lletra incorrecte";
         } else {
             boolean exist = false;
-            for (int i = 0; i < paraulaSecreta.length; i++) {
-                if (paraulaSecreta[i] == letra.charAt(0)) {
-                    exist = true;
-                    palabraEndevinada[i] = letra;
+
+            for (int i = 0; i < paraulaSecreta.length()) {
+
+                for (int i = 0; i < paraulaSecreta.length(); i++) {
+
+                    if (paraulaSecreta[i] == letra.charAt(0)) {
+                        exist = true;
+                        String[] palabraEndevinada;
+                        palabraEndevinada[i] = letra;
+                    }
                 }
-            }
 
-            if (!exist) {
-                restarIntent();
-            }
+                if (!exist) {
+                    restarIntent();
+                }
 
+            }
+            return "";
         }
-        return "";
+
+        private void restarIntent() {
+        }
+
+        public boolean verificar(String letra) {
+        }
+
+        public void inicialitzarPartida(String paraula, int parseInt) {
+        }
+
+        public boolean imprimirVides() {
+        }
+
+        public int getIntents() {
+        }
+
+        public char[] getParaulaSecreta() {
+        }
+
+        public Object[] getPalabraEndevinada() {
+        }
     }
 
-}
+    private void restarIntent() {
+    }
+
+    public void inicialitzarPartida(String paraula, int parseInt) {
+    }
