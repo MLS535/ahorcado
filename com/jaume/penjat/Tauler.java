2929b11bf50e01e8d5d84d24636025fdf8c796a9
Toni <garcia.t20@gmail.com>
2021-02-03 16:42:51 +0100

    verificar method in teuler class

--- com/jaume/penjat/Tauler.java
+++ com/jaume/penjat/Tauler.java
@@ -2,4 +2,24 @@ package com.jaume.penjat;
 
 public class Tauler {
 
+    public String verificar(String letra) {
+        if (letra.lenght() > 1) {
+            return "Lletra incorrecte";
+        } else {
+            boolean exist = false;
+            for (int i = 0; i < paraulaSecreta.length) {
+                if (paraulaSecreta[i] == letra.charAt(0)) {
+                    exist = true;
+                    palabraEndevinada[i] = letra;
+                }
+            }
+
+            if (!exist) {
+                restarIntent();
+            }
+
+        }
+        return "";
+    }
+
 }
