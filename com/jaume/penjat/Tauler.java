b12b1f8611d2e2f31296fe9f9f3ca683f7967824
Maite <mladaria@cifpfbmoll.eu>
2021-02-03 16:52:03 +0100

    He ficat nous metodos

--- com/jaume/penjat/Tauler.java
+++ com/jaume/penjat/Tauler.java
@@ -1,15 +1,22 @@
 package com.jaume.penjat;
 
 public class Tauler {
+    public boolean hasGuanyat() {
+    }
+
+    public boolean imprimir() {
+    }
 
     public String verificar(String letra) {
-        if (letra.lenght() > 1) {
+        if (letra.length() > 1) {
             return "Lletra incorrecte";
         } else {
             boolean exist = false;
-            for (int i = 0; i < paraulaSecreta.length) {
+            String paraulaSecreta;
+            for (int i = 0; i < paraulaSecreta.length()) {
                 if (paraulaSecreta[i] == letra.charAt(0)) {
                     exist = true;
+                    String[] palabraEndevinada;
                     palabraEndevinada[i] = letra;
                 }
             }
@@ -22,4 +29,24 @@ public class Tauler {
         return "";
     }
 
+    private void restarIntent() {
+    }
+
+    public boolean verificar(String letra) {
+    }
+
+    public void inicialitzarPartida(String paraula, int parseInt) {
+    }
+
+    public boolean imprimirVides() {
+    }
+
+    public int getIntents() {
+    }
+
+    public char[] getParaulaSecreta() {
+    }
+
+    public Object[] getPalabraEndevinada() {
+    }
 }
